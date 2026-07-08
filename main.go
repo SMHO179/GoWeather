4001f8fe7a828dd0a91403e4db28aac1c4790d2e
SMohammad-Molanezhad <SMHO11@protonmail.com>
2026-07-08 18:36:26 +0330

    Update main.go

--- main.go
+++ main.go
@@ -6,25 +6,34 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 )
 
 const baseURL = "https://wttr.in"
 
+var client = &http.Client{
+	Timeout: 10 * time.Second,
+}
+
 func fetchWeather(location string) (string, error) {
 	url := fmt.Sprintf("%s/%s?format=3", baseURL, location)
 
-	resp, err := http.Get(url)
+	resp, err := client.Get(url)
 	if err != nil {
 		return "", err
 	}
 	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("weather service returned status: %s", resp.Status)
+	}
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return "", err
 	}
 
-	return string(body), nil
+	return strings.TrimSpace(string(body)), nil
 }
 
 func main() {
@@ -34,11 +43,13 @@ func main() {
 		location = strings.Join(os.Args[1:], " ")
 	}
 
+	location = strings.TrimSpace(location)
+
 	weather, err := fetchWeather(location)
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 
-	fmt.Println("🌦 Weather:", weather)
+	fmt.Printf("Weather in %s:\n%s\n", location, weather)
 }
